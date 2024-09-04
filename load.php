e41086c3f65dcdb9b70d83c291bd3a098874f9bb
Ethan Bwibo <ethan.bwibo@strathmore.edu>
2024-09-04 14:48:57 +0300

    EOD 2

--- load.php
+++ load.php
@@ -2,4 +2,15 @@
 
 require_once "user_details.php";
 $obj = new user_details();
+
+$arr = ["black", "white", "green", "red"];
+
+foreach($arr as $color){
+  print $color . "<br>";
+}
+
+print dirname(__FILE__);
+print "<br>":
+print "<br>";
+print $_SERVER["PHP_SELF"];
 ?>
