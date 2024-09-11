af6ad8660a08e36fbc200c96fdc3098bf3e0eda0
Ethan Bwibo <enbwibo@gmail.com>
2024-09-11 15:12:33 +0300

    EOD 4

--- Contents/contents.php
+++ Contents/contents.php
@@ -3,14 +3,17 @@ class contents{
     public function main_content(){
         ?>
         <div class="row">
-        <div class="column">
-
+        <div class="content">
+            <h1>Main Content</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <?php
     }
     public function side_bar(){
         ?>
         <div class="side_bar">
+            <h1>Side Bar</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
         </div>
         </div>
