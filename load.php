5db582be3db9d330c9b3cb2262b17d36495e9bb0
Ethan Bwibo <enbwibo@gmail.com>
2024-10-01 22:27:22 +0300

    load.php

--- load.php
+++ load.php
@@ -1,3 +1,40 @@
+<?php
+session_start();
+require "includes/constants.php";
+require "includes/dbConnection.php";
+
+// Class Auto Load 
+
+function classAutoLoad($classname){
+
+    $directories = ["Contents", "Layouts", "Menus", "Forms", "Processes", "Global"];
+
+    foreach($directories AS $dir){
+        $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
+        if(file_exists($filename) AND is_readable($filename)){
+            require_once $filename;
+        }
+    }
+}
+
+spl_autoload_register('classAutoLoad');
+
+    $ObjGlob = new fncs();
+
+// Create instances of all classes
+    $ObjLayouts = new layout();
+    $ObjMenus = new menu();
+    $ObjHeadings = new heading();
+    $ObjCont = new content();
+    $ObjForm = new user_forms();
+    $conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
+
+
+// Create process instances
+
+    $ObjAuth = new auth();
+    $ObjAuth->signup($conn, $ObjGlob);
+
 <!-- <php
 function classAutoLoad($classname){
     $directories = ["Contents", "Layouts", "Menus"];
@@ -60,46 +97,3 @@ $conn = new dbConnection(DB_NAME, HOSTNAME, DBPORT, USERNAME, DBPASS, DBNAME);
 // }
 
 ?> -->
-
-
-
-
-
-
-<?php
-session_start();
-require "includes/constants.php";
-require "includes/dbConnection.php";
-
-// Class Auto Load 
-
-function classAutoLoad($classname){
-
-    $directories = ["Contents", "Layouts", "Menus", "Forms", "Processes", "Global"];
-
-    foreach($directories AS $dir){
-        $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
-        if(file_exists($filename) AND is_readable($filename)){
-            require_once $filename;
-        }
-    }
-}
-
-spl_autoload_register('classAutoLoad');
-
-    $ObjGlob = new fncs();
-
-// Create instances of all classes
-    $ObjLayouts = new layout();
-    $ObjMenus = new menu();
-    $ObjHeadings = new heading();
-    $ObjCont = new content();
-    $ObjForm = new user_forms();
-    $conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
-
-
-// Create process instances
-
-    $ObjAuth = new auth();
-    $ObjAuth->signup($conn, $ObjGlob);
-
