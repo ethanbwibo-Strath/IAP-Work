af612518ff6056da7faf0eb7e47507ec0147a7ac
Ethan Bwibo <enbwibo@gmail.com>
2024-10-01 23:29:14 +0300

    text in heading

--- Contents/heading.php
+++ Contents/heading.php
@@ -5,7 +5,7 @@ class heading{
         <div class="p-5 mb-4 bg-body-tertiary rounded-3">
             <div class="container-fluid py-2">
                 <h1 class="display-5 fw-bold">Assignment II</h1>
-                <p class="col-md-12 fs-6">This is an individual assignment. We assume that you already know how to set up a localhost server(s): & MySQL database and Apache web servers.<br>If not, your instructor will assist you to set it up.</p>
+                <p class="col-md-12 fs-6"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ut id quasi optio corporis, accusantium nulla blanditiis itaque expedita dolores rerum. Non numquam possimus illum doloribus tenetur soluta laboriosam perferendis. <br>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                 <button class="btn btn-primary btn-lg" type="button">Example button</button>
             </div>
       </div>
