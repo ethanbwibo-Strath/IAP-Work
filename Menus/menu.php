8fc2647992f1d6eef5db37aa06cba1481f799ce9
Ethan Bwibo <enbwibo@gmail.com>
2024-10-02 09:12:42 +0300

    Finalizing

--- Menus/menu.php
+++ Menus/menu.php
@@ -61,7 +61,7 @@ class menus{
       <header class="pb-3 mb-4 border-bottom">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Ninth navbar example">
             <div class="container-xl">
-              <a class="navbar-brand" href="./">ICS Demo</a>
+              <a class="navbar-brand" href="./">IAP Project</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
